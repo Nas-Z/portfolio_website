60cd2e66d6dc2ed135b75ffefe1618717f27fd1f
NAZ <112337066+Nas-Z@users.noreply.github.com>
2024-06-11 12:36:18 +0300

    Add Gameboxd Project

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ function toggleMenu(){
     icon.classList.toggle("open")
 }
 
-function playVideo() {
-    const video = "./assets/JAWLAH.mp4";
+function playVideo(String) {
+    const video = String;
     window.open(video, '_blank');
     
 }
+
+document.getElementById('showAllBtn').addEventListener('click', function() {
+    const hiddenProjects = document.querySelector('.hidden-projects');
+    const contactSection = document.getElementById('contact');
+    if (hiddenProjects.style.display === 'none'){
+        hiddenProjects.style.display = 'flex'
+        this.textContent = 'Show Less'
+        contactSection.style.marginTop = '35rem';
+
+    }
+    else{
+        hiddenProjects.style.display = 'none'
+        this.textContent = 'Show All'
+        contactSection.style.marginTop = '0rem';
+    }
+
+  });
